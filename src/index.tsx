5f7e4bec790971dcb8578a3b8bc7e835b214857b
Ruslan_Vasilev <112714732+RuslanSmap@users.noreply.github.com>
2025-05-04 17:32:22 +0300

    tests objects

--- src/index.tsx
+++ src/index.tsx
@@ -4,12 +4,16 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import {splitIntoWords} from "./01-hello-tests/01";
+import {student} from "./02/02";
 
 const sentense = 'Hello my friends!';
 const result = splitIntoWords(sentense);
-console.log(result[0] === 'hello');
-console.log(result[1] === 'my');
-console.log(result[2] === 'friend');
+
+// console.log(result[0] === 'hello');
+// console.log(result[1] === 'my');
+// console.log(result[2] === 'friend');
+
+// console.log(student)
 
 const root = ReactDOM.createRoot(
     document.getElementById('root') as HTMLElement
